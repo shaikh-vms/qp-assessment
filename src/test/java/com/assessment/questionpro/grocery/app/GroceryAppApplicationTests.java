be858f120e11a2629df906cdf0f18077541aa8a4
shaikh-vms <shaikhvms@gmail.com>
2025-02-08 17:21:12 +0530

    init: initial commit

---
+++ src/test/java/com/assessment/questionpro/grocery/app/GroceryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.questionpro.grocery.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
